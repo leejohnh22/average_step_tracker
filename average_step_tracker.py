03be1e4ce8f4c267671d8f5372bf48147a27cc24
John Lee <leejohnh22@gmail.com>
2019-04-13 11:57:48 -0500

    4-13-2019
    Handles text files with less data than expected gracefully - but can be improved

---
+++ average_step_tracker.py
@@ -0,0 +1,205 @@
+# John Lee		4/11/2019
+# Last Update	4/13/2019
+# Program Exercise 6-12
+# Calculate the average of steps taken each month and print to standard out
+
+# Input
+#	steps_text
+# Output
+#	steps_in_month
+
+# Import libraries
+import os
+
+# Define Constants
+MONTHS_IN_YEAR = 12
+DAYS_IN_JANUARY = 31
+DAYS_IN_FEBRUARY = 28
+DAYS_IN_MARCH = 31
+DAYS_IN_APRIL = 30
+DAYS_IN_MAY = 31
+DAYS_IN_JUNE = 30
+DAYS_IN_JULY = 31
+DAYS_IN_AUGUST = 31
+DAYS_IN_SEPTEMBER = 30
+DAYS_IN_OCTOBER = 31
+DAYS_IN_NOVEMBER = 30
+DAYS_IN_DECEMBER = 31
+DAYS_IN_YEAR = DAYS_IN_JANUARY + DAYS_IN_FEBRUARY + DAYS_IN_MARCH + DAYS_IN_APRIL + DAYS_IN_MAY + DAYS_IN_JUNE + DAYS_IN_JULY + DAYS_IN_AUGUST + DAYS_IN_SEPTEMBER + DAYS_IN_OCTOBER + DAYS_IN_NOVEMBER + DAYS_IN_DECEMBER
+
+def main():
+	# Initialize variables
+	text_file = 'steps.txt'
+	num_months_in_file = 0
+	cur_month = 1
+	day_count = 0
+	total_line_counter = 0
+	num_steps = 0
+	num_total_steps = 0
+	average_step_count = 0
+
+	# Define dictionary to hold average number of steps taken each month
+	steps_in_month = {
+	1 : 0,
+	2 : 0,
+	3 : 0,
+	4 : 0,
+	5 : 0,
+	6 : 0,
+	7 : 0,
+	8 : 0,
+	9 : 0,
+	10 : 0,
+	11 : 0,
+	12 : 0
+	}
+
+	# Define dictionary that holds number of days for each month
+	days_in_month = {
+	1 : DAYS_IN_JANUARY,
+	2 : DAYS_IN_FEBRUARY,
+	3 : DAYS_IN_MARCH,
+	4 : DAYS_IN_APRIL,
+	5 : DAYS_IN_MAY,
+	6 : DAYS_IN_JUNE,
+	7 : DAYS_IN_JULY,
+	8 : DAYS_IN_AUGUST,
+	9 : DAYS_IN_SEPTEMBER,
+	10 : DAYS_IN_OCTOBER,
+	11 : DAYS_IN_NOVEMBER,
+	12 : DAYS_IN_DECEMBER
+	}
+
+	cumulative_days_by_month = {
+	1 : DAYS_IN_JANUARY,
+	2 : DAYS_IN_JANUARY + DAYS_IN_FEBRUARY,
+	3 : DAYS_IN_JANUARY + DAYS_IN_FEBRUARY + DAYS_IN_MARCH,
+	4 : DAYS_IN_JANUARY + DAYS_IN_FEBRUARY + DAYS_IN_MARCH + DAYS_IN_APRIL,
+	5 : DAYS_IN_JANUARY + DAYS_IN_FEBRUARY + DAYS_IN_MARCH + DAYS_IN_APRIL + DAYS_IN_MAY,
+	6 : DAYS_IN_JANUARY + DAYS_IN_FEBRUARY + DAYS_IN_MARCH + DAYS_IN_APRIL + DAYS_IN_MAY + DAYS_IN_JUNE,
+	7 : DAYS_IN_YEAR - DAYS_IN_DECEMBER - DAYS_IN_NOVEMBER - DAYS_IN_OCTOBER - DAYS_IN_SEPTEMBER - DAYS_IN_AUGUST,
+	8 : DAYS_IN_YEAR - DAYS_IN_DECEMBER - DAYS_IN_NOVEMBER - DAYS_IN_OCTOBER - DAYS_IN_SEPTEMBER,
+	9 : DAYS_IN_YEAR - DAYS_IN_DECEMBER - DAYS_IN_NOVEMBER - DAYS_IN_OCTOBER,
+	10 : DAYS_IN_YEAR - DAYS_IN_DECEMBER - DAYS_IN_NOVEMBER,
+	11 : DAYS_IN_YEAR - DAYS_IN_DECEMBER,
+	12 : DAYS_IN_YEAR
+	}
+
+	try:
+		# Check if file is empty (0 bytes in size)
+		if os.stat(text_file).st_size == 0:
+			print("File is empty.")
+
+		# If not empty, open file in read mode
+		else:
+			# Calculate number of lines
+			with open(text_file, 'r') as file:
+				num_lines = sum(1 for line in file)
+
+				# Calculate number of months data in file if number of lines is less than 12 months
+				for i in range(1, len(cumulative_days_by_month) + 1):
+					if i < len(cumulative_days_by_month):
+						if (num_lines > cumulative_days_by_month[i]) and (num_lines < cumulative_days_by_month[i + 1]):
+							num_months_in_file = i
+					# else we know that there are at least 11 months of data
+					else:
+						num_months_in_file = len(cumulative_days_by_month)
+
+			with open(text_file, 'r') as step_file:
+				# While month_count is less than months_in_year
+				while cur_month <= num_months_in_file:
+					# While day_count is less than days_in_month of current month read line in file, strip \n, convert to int
+					while day_count <= days_in_month[cur_month]:
+						line = step_file.readline()
+						# If not an empty string process it
+						if line.strip():
+							num_steps = int(line.strip())
+
+						# Add steps taken that day to total step count
+						num_total_steps += num_steps
+
+						# Increment day_count by one for each line read
+						day_count += 1
+
+					# When current month is done being counted, calculate average step count and store in average_step_count
+					average_step_count = calc_avg_step_in_month(days_in_month[cur_month], num_total_steps)
+					steps_in_month[cur_month] = average_step_count
+
+					# Increment month count
+					cur_month += 1
+
+					# Reset day count and step count
+					day_count = 0
+					num_steps = 0
+					num_total_steps = 0
+
+			# Format output
+			print()
+			# Output WARNING if number of days in file is less than one year
+			if num_lines < DAYS_IN_YEAR:
+				print("\tWARNING:\tNumber of days in %s contains less than the number of days in a year (%i)." % (text_file, DAYS_IN_YEAR))
+				print("\t\t\tTable below may contain inaccurate averages.")
+			# Output WARNING if number of days in file is more than one year
+			elif num_lines > DAYS_IN_YEAR:
+				print("\tWARNING:\tNumber of days in %s contains more than the number of days in a year." % text_file)
+				print("\t\t\tTable below will contain averages for the first %i days." % DAYS_IN_YEAR)
+
+			# Print monthly averages to stdout
+			print_step_average_monthly(steps_in_month[1], steps_in_month[2], steps_in_month[3], steps_in_month[4], steps_in_month[5], steps_in_month[6], steps_in_month[7], steps_in_month[8], steps_in_month[9], steps_in_month[10], steps_in_month[11], steps_in_month[12])
+
+			# File Closed
+
+	# Raise exception if file does not exist
+	except FileNotFoundError:
+		print("%s not found. Please verify %s exists in this directory." % (text_file, text_file))
+
+	# Raise exception if line read does not contain a digit
+	except ValueError:
+		print("%s must contain numerical values only. Make sure the file has no empty lines" % text_file)
+
+	# Raise exception if keyboard interrupted
+	except KeyboardInterrupt:
+		print("Program interrupted by user.")
+
+	except MemoryError:
+		print("Out of memory. Please verify %s does not contain too many lines and there is sufficient memory available" % text_file)
+
+# Define function to calculate average steps for the month
+def calc_avg_step_in_month(num_days, total_steps):
+	try:
+		# Calculate average step count
+		average_step_count = total_steps / num_days
+
+		# Return average step count
+		return average_step_count
+
+	# Raise exception if float conversion fails
+	except FloatingPointError:
+		print("File must contain numerical values only.")
+
+	# Raise exception if count is 0
+	except ZeroDivisionError:
+		print("File must contain more than one number.")
+
+# Define function to print out average number of steps for each month, format as integer with comma seperators
+def print_step_average_monthly(jan_step_avg, feb_step_avg, mar_step_avg, apr_step_avg, may_step_avg, jun_step_avg, jul_step_avg, aug_step_avg, sep_step_avg, oct_step_avg, nov_step_avg, dec_step_avg):
+	print()
+	print("\tAverage Step Count for each Month : ")
+	print("\t____________________________________")
+	print()
+	print("\tJanuary\t\t", format(jan_step_avg, ',.0f'))
+	print("\tFebruary\t", format(feb_step_avg, ',.0f'))
+	print("\tMarch\t\t", format(mar_step_avg, ',.0f'))
+	print("\tApril\t\t", format(apr_step_avg, ',.0f'))
+	print("\tMay\t\t", format(may_step_avg, ',.0f'))
+	print("\tJune\t\t", format(jun_step_avg, ',.0f'))
+	print("\tJuly\t\t", format(jul_step_avg, ',.0f'))
+	print("\tAugust\t\t", format(aug_step_avg, ',.0f'))
+	print("\tSeptember\t", format(sep_step_avg, ',.0f'))
+	print("\tNovember\t", format(nov_step_avg, ',.0f'))
+	print("\tDecember\t", format(dec_step_avg, ',.0f'))
+
+# Run program
+main()
+
+# END OF PROGRAM
